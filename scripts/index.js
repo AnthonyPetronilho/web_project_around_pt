a8ac560d2bb47927afbf492e36c5ad037d96adfe
Anthony <anthony_celso@hotmail.com>
2025-11-26 10:54:04 -0300

    Etapa 2. Modal de perfil

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,49 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close");
+const modal = document.querySelector("#edit-popup");
+
+function openModal(modal) {
+  modal.classList.add("popup_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("popup_is-opened");
+}
+
+editButton.addEventListener("click", () => openModal(modal));
+closeButton.addEventListener("click", () => closeModal(modal));
+
+function fillProfileForm() {
+  const currentName = document.querySelector(".profile__title").textContent;
+  const currentAbout = document.querySelector(
+    ".profile__description"
+  ).textContent;
+
+  document.querySelector("#name-input").value = currentName;
+  document.querySelector("#about-input").value = currentAbout;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(modal);
+}
+
+editButton.addEventListener("click", handleOpenEditModal);
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameValue = nameInput.value;
+  let aboutValue = jobInput.value;
+
+  profileTitle.textContent = nameValue;
+  profileDescription.textContent = aboutValue;
+
+  closeModal(modal);
+}
