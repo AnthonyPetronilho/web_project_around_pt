662200b5303703d6e10a18e3f2df0d512b1932e9
Anthony <anthony_celso@hotmail.com>
2026-01-12 14:13:21 -0300

    Ultimas atualizações

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,6 +2,33 @@ const imageModal = document.querySelector("#image-popup");
 const popupImage = imageModal?.querySelector(".popup__image");
 const popupCaption = imageModal?.querySelector(".popup__caption");
 
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+  },
+  {
+    name: "Parque Nacional Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
+];
+
 const validationConfig = {
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
@@ -34,6 +61,7 @@ function handleEscClose(evt) {
 
 export {
   imageModal,
+  initialCards,
   validationConfig,
   handleImageClick,
   openModal,
